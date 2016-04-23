8840f25c8c8160a255481b224031339772900246
root <root@fabrizio-laptop-debian>
2016-04-23 19:34:13 +0200

    asdsad

--- showBookings.php
+++ showBookings.php
@@ -87,9 +87,6 @@ tbody td, thead th {
         $conn = new PDO("mysql:host=$servername;dbname=sitooriginale", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // $select_all = $conn->prepare("SELECT * FROM prenotazione;");
-        // $select_all->execute();
-        // $result = $select_all->setFetchMode(PDO::FETCH_ASSOC);
         $result = $conn->query("SELECT * FROM prenotazione");
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
           echo "<tr>";
