a4bc142d92d8df07a01ce4a32ffd4dd0d6b48da0
HoberMallow <and22591@gmail.com>
2016-04-23 19:32:04 +0200

    Modified login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ Login
 </title>
 
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
 integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 <style type="text/css">
@@ -68,10 +68,9 @@ body {
           </label>
         </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-        <h6>Non sei Registrato? <a href="">Sign up!</a></h6>
       </form>
 
-    </div> 
-    
+    </div>
+
 </body>
 </html>
